455d569d125ec4144a1f753a45d0b29e463b3b6d
Rodrigo Pérez <rodrigop_23@outlook.es>
2024-10-15 10:05:48 -0500

    chore(api-gateway): - Delete unused app resources - Add config file for envs - Add env template

---
+++ apps/server/api-gateway/src/common/config/envs.ts
@@ -0,0 +1,40 @@
+import { config } from 'dotenv';
+import { expand } from 'dotenv-expand';
+
+import { ZodError, z } from 'zod';
+
+const EnvSchema = z.object({
+  PORT: z
+    .string()
+    .min(1)
+    .transform((v) => parseInt(v, 10)),
+  API_GATEWAY_PREFIX: z.string().min(1),
+  NATS_SERVER: z
+    .string()
+    .min(1)
+    .transform((v) => v.split(',')),
+});
+
+export type EnvSchema = z.infer<typeof EnvSchema>;
+
+expand(config());
+
+try {
+  EnvSchema.parse(process.env);
+
+  console.log(EnvSchema.parse(process.env));
+} catch (error) {
+  if (error instanceof ZodError) {
+    let message = 'Missing required values in .env:\n';
+    error.issues.forEach((issue) => {
+      message += issue.path[0] + '\n';
+    });
+    const e = new Error(message);
+    e.stack = '';
+    throw e;
+  } else {
+    console.error(error);
+  }
+}
+
+export default EnvSchema.parse(process.env);
--- apps/server/api-gateway/src/main.ts
+++ apps/server/api-gateway/src/main.ts
@@ -3,20 +3,22 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
-import { AppModule } from './app/app.module';
+import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common/services/logger.service';
+import { envs } from './common/config';
 
 async function bootstrap() {
+  const logger = new Logger('Main-Gateway');
+
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
-  );
+
+  app.setGlobalPrefix(envs.API_GATEWAY_PREFIX);
+
+  await app.listen(envs.PORT);
+
+  logger.log(`Gateway running on port ${envs.PORT}`);
 }
 
 bootstrap();
