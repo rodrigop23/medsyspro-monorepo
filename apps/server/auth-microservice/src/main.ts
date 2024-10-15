8103a88d4ed2b655cb183b401785e8a2082a79af
Rodrigo Pérez <rodrigop_23@outlook.es>
2024-10-15 10:06:59 -0500

    chore(auth-microservice): - Add configuration to use NATS as transport - Add config file for envs - Update packages and gitignore

--- apps/server/auth-microservice/src/main.ts
+++ apps/server/auth-microservice/src/main.ts
@@ -3,20 +3,24 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
-import { AppModule } from './app/app.module';
+import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { envs } from './config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.NATS,
+      options: {
+        servers: envs.NATS_SERVER,
+      },
+    }
   );
+
+  await app.listen();
 }
 
 bootstrap();
