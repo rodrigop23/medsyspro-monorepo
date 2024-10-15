6ecfa7d6c11f5b5c4085c9d77fb8ebb51d07ac3e
Rodrigo Pérez <rodrigop_23@outlook.es>
2024-10-15 15:57:05 -0500

    chore: add multiples microservices

---
+++ apps/server/medical-history-microservice/jest.config.ts
@@ -0,0 +1,11 @@
+export default {
+  displayName: 'medical-history-microservice',
+  preset: '../../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory:
+    '../../../coverage/apps/server/medical-history-microservice',
+};
