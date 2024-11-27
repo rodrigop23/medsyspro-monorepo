0c13a7a3cd3bf0e9cd2278b198b288129d6823c5
Rodrigo Pérez <rodrigop_23@outlook.es>
2024-11-27 09:35:30 -0500

    feat: add prisma client in auth-ms

--- apps/auth-ms/src/app.module.ts
+++ apps/auth-ms/src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { PrismaService } from './db-client/db-client.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule],
+  controllers: [],
+  providers: [PrismaService],
 })
 export class AppModule {}
