d3221416fa16b463d35014b7759b627d1270eff6
Rodrigo Pérez <rodrigop_23@outlook.es>
2024-11-27 20:31:06 -0500

    wip(auth-ms): add create user dto

--- apps/auth-ms/src/auth/dto/create-auth.dto.ts
+++ apps/auth-ms/src/auth/dto/create-auth.dto.ts
@@ -1,1 +1,50 @@
-export class CreateAuthDto {}
+import {
+  IsDate,
+  IsEmail,
+  IsEnum,
+  IsString,
+  IsStrongPassword,
+} from 'class-validator';
+import { DocumentTypeList, GenderList } from '../enum/user.enum';
+
+export class CreateAuthDto {
+  @IsString()
+  name: string;
+
+  @IsString()
+  firstSurname: string;
+
+  @IsString()
+  secondSurname: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsStrongPassword({
+    minLength: 8,
+    minLowercase: 1,
+    minNumbers: 1,
+    minSymbols: 1,
+    minUppercase: 1,
+  })
+  password: string;
+
+  @IsEnum(GenderList, {
+    message: `Valid gender values are ${GenderList}`,
+  })
+  gender: string;
+
+  @IsDate()
+  birthdate: Date;
+
+  @IsEnum(DocumentTypeList, {
+    message: `Valid document type values are ${DocumentTypeList}`,
+  })
+  documentType: string;
+
+  @IsString()
+  documentNumber: string;
+
+  @IsString()
+  phone: string;
+}
---
+++ apps/auth-ms/src/auth/enum/user.enum.ts
@@ -0,0 +1,11 @@
+// import { DocumentType, Gender } from '@repo/user-db-prisma';
+
+export const DocumentTypeList = [
+  // DocumentType.DNI,
+  // DocumentType.PASSPORT,
+  // DocumentType.CE,
+];
+
+export const GenderList = [
+  // Gender.MALE, Gender.FEMALE, Gender.OTHER
+];
