39acc215a0609236d6bbd140d5e54fde526759b0
SEBASTIAN GUZMAN SOTO <20200981@aloe.ulima.edu.pe>
2024-11-28 01:57:02 -0500

    cambios ms resultados

--- apps/resultados-ms/src/infrastructure/database/result.repository.impl.ts
+++ apps/resultados-ms/src/infrastructure/database/result.repository.impl.ts
@@ -1,4 +1,23 @@
 import { ResultRepository } from "src/domain/repositories/result.repository";
 import { Result } from "src/domain/entities/result.entity";
-import {PrismaService} from "src/db-client/db-client.service"
+import {PrismaService} from './prisma.service';
 
+
+export class ResultRepositoryImpl implements ResultRepository {
+    constructor(private readonly prisma: PrismaService) {}
+  
+    async findResultsByPatient(patientId: string): Promise<Result[]> {
+      const results = await this.prisma.result.findMany({
+        where: { patientId },
+      });
+      return results.map(
+        (r) => new Result(r.id, r.patientId, r.type, r.filePath, r.createdAt),
+      );
+    }
+  
+    async downloadResult(id: string): Promise<Result> {
+      const result = await this.prisma.result.findUniqueOrThrow({ where: { id } });
+      return new Result(result.id, result.patientId, result.type, result.filePath, result.createdAt);
+    }
+  }
+  
